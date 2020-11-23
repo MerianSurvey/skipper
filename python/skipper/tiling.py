b765d04d629ddcd5a013fae804687350fadbe174
Shany Danieli <shany.danieli@yale.edu>
2020-11-23 16:37:17 -0500

    added the option to include a 4th pass analysis for the sky coverage histogram

--- python/skipper/tiling.py
+++ python/skipper/tiling.py
@@ -1,8 +1,16 @@
-from __future__ import print_function
+#!/usr/bin/env python
+"""
+Calculate sky coverage for a decam tiling.
+"""
+__author__ = "Shany Danieli"
+
+
+# from __future__ import print_function
 import matplotlib
 matplotlib.use('Agg')
 matplotlib.rc('text', usetex=True)
 matplotlib.rc('font', family='serif')
+from os.path import join,dirname,abspath
 import pylab as plt
 import numpy as np
 import fitsio
@@ -12,21 +20,32 @@ from astrometry.util.util import wcs_pv2sip_hdr, Tan
 from astrometry.util.resample import *
 from astrometry.libkd.spherematch import match_radec
 from astrometry.util.plotutils import *
-# from astrometry.blind.plotstuff import *
 from astrometry.plot.plotstuff import *
 from astrometry.util.util import anwcs_new_sip
 
+def get_datadir():
+    return join(dirname(dirname(dirname(abspath(__file__)))),'data')
 
+def get_datafile(filename):
+    dirname = get_datadir()
+    filepath = os.path.join(dirname,filename)
+
+    if not os.path.exists(filepath):
+        msg = "File does not exists: %s"%filepath
+        logging.warn(msg)
+    else:
+        return filepath            
 
 def decam():
     
     plt.figure(figsize=(4,3))
     plt.subplots_adjust(left=0.15, right=0.99, top=0.99, bottom=0.15)
     
-    T = fits_table('../data/decam-tiles_obstatus.fits')
-    # T = fits_table('../data/decam-tiles-bliss-v1.fits')
+    datadir = join(dirname(dirname(dirname(abspath(__file__)))),'data')
+
+    # T = fits_table(datadir+'/decam-tiles_obstatus.fits')
+    T = fits_table(datadir+'/decam-tiles-decals-merian.fits')
     T.rename('pass', 'passnum')
-    # ra,dec = 0.933, 0.
     ra,dec = 0.933, 0.
     I,J,d = match_radec(T.ra, T.dec, ra, dec, radius_in_deg=5.) #2.8) 
     print(len(I), 'tiles near 0,0')
@@ -35,12 +54,12 @@ def decam():
     print('dists:', d)
     print('Passes:', T.passnum)
 
-    F = fitsio.FITS(os.path.join(os.path.dirname(__file__), '../data/decam.wcs')) 
+    F = fitsio.FITS(os.path.join(datadir+'/decam.wcs')) 
     wcs = []
     for i in range(1, len(F)):
         hdr = F[i].read_header()
         wcs.append(wcs_pv2sip_hdr(hdr, W=2046, H=4094))
-    
+
     W,H = 5000, 5000
     pixsc = 4./3600.
     targetwcs = Tan(ra, dec, W/2.+0.5, H/2.+0.5, -pixsc, 0., 0., pixsc,
@@ -48,7 +67,7 @@ def decam():
     II = np.lexsort((T.dist, T.passnum))
     
     # This is for making the (vector) PDF format tiling images.
-    for maxit in [0, 6, 30, 31, 37, 61, 62, 68, 90]:
+    for maxit in [0, 6, 30, 31, 37, 61, 62, 68, 90, 91, 92, 119]:
         #mx = { 1: 2, 2: 4, 3: 6 }[t.passnum]
         plot = Plotstuff(outformat='pdf', ra=ra, dec=dec, width=W*pixsc,
                          size=(W,H), outfn='tile-%02i.pdf' % maxit)
@@ -59,6 +78,10 @@ def decam():
         out = plot.outline
         out.fill = True
         out.stepsize = 1024.
+        print('maxit: '+str(maxit))
+        if maxit >100:
+            print('YES!!!!!')
+            plot.color = 'pink'
         plot.color = 'black'
         plot.alpha = 0.4
         plot.apply_settings()
@@ -88,6 +111,7 @@ def decam():
                 #traceback.print_exc()
                 continue
             cov[Yo,Xo] += 1
+
     
         if it in [0, 6, 30, 31, 37, 61, 62, 68, 90]:
             mx = { 1: 2, 2: 4, 3: 6, 4:8 }[t.passnum]
@@ -100,8 +124,12 @@ def decam():
             plt.imsave('tile-%02i.png' % it, cov, origin='lower', vmin=0, vmax=mx, cmap=antigray)
             #plt.imsave('tile-%02i.pdf' % it, cov, origin='lower', vmin=0, vmax=mx, cmap=antigray, format='pdf')
     
-        if it in [30, 61, 90]:
+        if it in [30, 61, 90, 119]:
             from collections import Counter
+            if it == 30:
+                print(cov.tolist())
+            quit()
+
             print('Coverage counts:', Counter(cov.ravel()).most_common())
             bins = -0.5 + np.arange(8)
             plt.clf()
@@ -134,4 +162,14 @@ def decam():
             plt.savefig('hist-%02i.pdf' % it)
 
 
-decam()
+
+if __name__ == "__main__":
+
+    decam()
+
+
+
+
+
+
+
