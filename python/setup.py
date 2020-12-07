50f616ff66cbf5f6a1ca9d31f0cadb34eee8488a
Erin Kado-Fong <kadofong@princeton.edu>
2020-12-07 15:00:22 -0500

    add pkg set-up tools

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup (
+    name = 'skipper',
+    version='0.0.1',
+    author='Shany Danieli, Erin Kado-Fong',
+    description='Observing strategy tools for Merian',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7'
+    )
