00fad87eb03482a117bed789da0eaae7e37b3380
Erin Kado-Fong <kadofong@princeton.edu>
2021-01-25 19:39:29 -0500

    [observe] add fundamental timezone and airmass tracking foundation

--- python/skipper/observe.py
+++ python/skipper/observe.py
@@ -4,6 +4,7 @@ Base functions to generate observing JSONs
 """
 __author__ = "Erin Kado-Fong"
 
+import datetime
 import numpy as np
 import pandas as pd
 import pytz
@@ -62,21 +63,82 @@ class ObsCatalog (object):
         open(fp, 'w').write(json_str)
 
 class ObservingSite ( object ):
+    '''
+    [Determine airmass as a function of time for a given observing catalog
+    and date.]
+    Defines an observing site from which we may calculate observing conditions,
+    given an target list and date.
+    '''
     def __init__ ( self, site='CTIO', timezone=None):
         if site=='CTIO':
             self.site = coordinates.EarthLocation ( lat='-30d10m10.78s',
                                                     lon='-70d48m23.49s',
                                                     height=2241.*u.m )
-            timezone='America/Santiago'
-
-            
+            self.timezone= pytz.timezone ( 'America/Santiago' )
         else:
+            # // if not CTIO, trust the user to put in an EarthLocation
+            # // and pytz.timezone ()
             self.site = site
-
-
+            if type(timezone) == str:
+                self.timezone = pytz.timezone(timezone)
+            else:
+                self.timezone = timezone
             
 
-    def get_utcoffset ( self, zone_name):
-        from pytz import timezone
-        
-        
+    def get_utcoffset ( self, date ):
+        '''
+        From a datetime object, return the UTC offset at the observing site
+        at that date/time.
+        '''
+        # // because of daylight savings time,
+        # // we need to calculate the UTC offset *after* we know the
+        # // observation date. So here I'll set up that machinery.
+        # // (We're actually observing during the daylight savings time
+        # // switch in 2021A, so we need to make sure this is robust)
+        return date.astimezone ( self.timezone ).utcoffset().total_seconds()/3600.
+
+    def define_obsframe ( self, obs_datetime, nstep=0.5, lim=6. ):
+        '''
+        For an individual night of an observing run, generate the
+        observing frame. The observing frame specifies observatory location
+        + time for +/- lim hours about the fiducial obs_datetime in steps of nstep
+        hours.
+        '''
+        utcoffset = self.get_utcoffset ( obs_datetime )
+        frame = np.arange ( -lim, lim+nstep/2., nstep) * u.hour
+        timeframe = Time ( obs_datetime ) + frame
+        obsframe = coordinates.AltAz ( obstime = timeframe, location=self.site)
+        return obsframe, utcoffset
+
+    def get_altitude ( self, target_coord, obsframe ):
+        '''
+        For an observing frame and target coordinates, get altitude as a
+        function of time
+        '''
+        alt = target_coord.transform_to ( obsframe )
+        return alt
+
+    def myownformatter ( self, dtobj ):
+        '''
+        There's a bug in matplotlib 2.0.0 that forcibly converts all datetime
+        objects to UTC when plotting, so we need to get around that with this hack
+        '''
+        return f'{dtobj.hour:02d}:00'
+    
+    def plot_altitude ( self, alt ):
+        fig, ax = plt.subplots(1,1,figsize=(8,6))
+
+        ax.plot ( alt.obstime.datetime, alt.alt, lw=3 )
+        ax.set_xlabel ("UTC")
+
+        twiny = ax.twiny ()
+        twiny.set_xlim ( ax.get_xlim() )
+        twiny.set_xticks ( ax.get_xticks() )
+        twiny.set_xticklabels ([ myownformatter(mdates.num2date(ct).astimezone(ctio.timezone))
+                                for ct in ax.get_xticks()] )
+        twiny.set_xlabel('local time')
+
+        xformatter = mdates.DateFormatter('%H:%M')
+        plt.gcf().axes[0].xaxis.set_major_formatter(xformatter)
+        #plt.gcf().axes[1].xaxis.set_major_formatter(xformatter)
+        plt.grid ()
