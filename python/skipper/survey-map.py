042e196bb4803d8ccd4cc787c0ba3a9868b73d84
Shany Danieli <shany.danieli@yale.edu>
2021-05-17 10:58:46 +0300

    start a script for survey map generation

---
+++ python/skipper/survey-map.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Base functions to generate and interact with survey maps 
+"""
+__author__ = "Shany Danieli"
+
+import numpy as np
+from astropy import coordinates
+from astropy import units as u
+from astropy.io import fits
+
+def generate_map(cat_dir, cat_name):
+	
+
+	
+
+
+def update_map(date, in_dirc, out_dirc):
+
