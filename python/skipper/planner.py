1518c1d0e17627941f38ad25883e0146b4c73785
Erin Kado-Fong <kadofong@princeton.edu>
2022-01-21 11:50:32 -0500

    add sigjson2022a and move fundamental functions to planner

---
+++ python/skipper/planner.py
@@ -0,0 +1,208 @@
+from asyncio.format_helpers import extract_stack
+import os
+import pytz
+import datetime
+import numpy as np
+import pandas as pd
+from astropy import coordinates
+from . import observe
+
+# \\ Define relevant timezones
+fmt = '%Y/%m/%d %I:%M %p'
+et = pytz.timezone("America/New_York")
+pt = pytz.timezone("America/Los_Angeles")
+ct = pytz.timezone("Asia/Chongqing")
+
+
+_BACKUP_FIELDS = ['SXDS','COSMOS']
+_backup_centers = {'SXDS':coordinates.SkyCoord ("35.739030633438745 -4.7489828727193775", unit='deg'),
+                    'COSMOS':coordinates.SkyCoord ("10h00m28.6s+02d12m21.0s") }
+
+def load_telemetry ( fname ):
+    return pd.read_csv(fname)
+
+
+def print_backupaltitudes (obs_start, obs_end, backup_fields=None):
+    '''
+    Plot backup field altitudes over the course of the (half) night
+    '''
+
+    if backup_fields is None:
+        backup_fields = _BACKUP_FIELDS
+    backup_centers = [ _backup_centers[name] for name in backup_fields ]
+    
+    ctio = observe.ObservingSite ()
+    obsframe = ctio.define_obsframe ( obs_start=obs_start, obs_end=obs_end )
+    alt_l = [ ctio.get_altitude(cc, obsframe) for cc in backup_centers]
+    
+    dtime = [ alt_l[0][ix].obstime.datetime for ix in range(len(alt_l[0]))]
+    hd = 'time (UTC)\t\t'
+    for iw in range(len(backup_centers)):
+        hd = f'{hd}{backup_fields[iw]}\t'
+    bfly = open('../resources/bfly.txt','r').read()
+    print(bfly)
+    print(hd)    
+    for iv in range(len(dtime)):
+        dt = dtime[iv]
+        st = f'{dt.strftime(fmt)}\t'
+        for iw in range(len(backup_centers)):            
+            airmass = alt_l[iw].secz[iv]
+            st = f'{st}{airmass:.2f}\t'
+        print(st) 
+ 
+def nextbackupscript ( tele, backup_fields=None ):
+    '''
+    Check which backup scripts have already been observed, and print out the next one
+    '''
+    if backup_fields is None:
+        backup_fields = _BACKUP_FIELDS
+    for name in backup_fields:
+        for filt in ['g','r']:
+            iu=0
+            while True:
+                fname = f'../json/backup_scripts/{name}_5minAGN_{filt}_{iu:02d}.json' 
+                if not os.path.exists ( fname ):
+                    iu += 1
+                    continue
+                elif iu > 10:
+                    raise ValueError ("No back-up scripts available! Need to regenerate")
+                else:
+                    json = pd.read_json ( fname )
+                    has_observed = np.in1d(json['object'].iloc[1:], tele['object']).any ()
+                    if has_observed:
+                        iu+=1
+                    else:
+                        print('------')
+                        print(f'Next script for {name} [{filt}] is {fname}')
+                        break
+                    
+def plan_tomorrow ( day, month, year, tele_fname, copilot_fname, mastercat, 
+                   whichfield=None,
+                   current_filter = None,
+                   current_field = None,
+                   current_slot = None, 
+                   is_queued=None,                  
+                   cut_at_contract=True,
+                   priorities=None,
+                   extra = None, # manual extra hours 
+                   **kwargs ):
+    '''
+    Plan "tomorrow" in the observing time frame (i.e. the next night that we will be observing)
+    
+    mastercat: pd.DataFrame
+        The catalog of all pointings in the field(s) to be observed 
+    cut_at_contract: bool, default=True
+        If True, the night end will occur, at latest, at 6:05 AM (Chilean work contract rule)
+    '''
+    tele = load_telemetry ( tele_fname )
+
+    # \\ figure out which field and filter we're going to be observing in,
+    if whichfield is not None:
+        mfilt, field, slot = whichfield (year,month,day)
+    else: 
+        mfilt = current_filter
+        field = current_field
+        slot = current_slot
+        
+    
+    # \\ QUICKFIX for 2021B TODO: fix in observe.py
+    if mfilt == 'N708':
+        skySB_0 = 21.
+        teff_min = 200.
+    elif mfilt == 'N540':
+        skySB_0 = 22.1
+        teff_min = 300.
+    else:
+        raise ValueError (f"the filter {mfilt} is not recognized!")
+
+    print(f"On {year}/{month}/{day}, we are observing {field} in {mfilt}")
+
+    if slot==0:
+        tag = 'full'
+    elif slot==1:
+        tag='first half'
+    elif slot==2:
+        tag = 'second half'
+    elif slot==3:
+        tag ='second 3/4'
+    elif slot==4:
+        tag = f'second half and the last {extra} hours of the first half'
+        
+    print(f'We are observing the {tag} of the night')
+    #exp_exposures = tele.query('(exptime>599.)&(object!="G09")').shape[0]
+    has_observed = np.in1d(mastercat['object'], tele['object'])
+    
+    # \\ also check for exposures that need to be reobserved
+    coo = observe.CopilotOutput ( copilot_fname, pointings=mastercat,  skySB_0 = skySB_0 )
+    reobs = coo.flag_for_reobservation ( min_teff = teff_min )
+    needs_reobservation = np.in1d(mastercat['object'], reobs)
+    print(f'{needs_reobservation.sum()} pointings in this catalog need reobservation!')
+    has_observed = has_observed & ~needs_reobservation
+    
+    
+    #assert has_observed.sum() == exp_exposures, "We have observed exposures that aren't in the master catalog?!"
+    ocat = observe.ObsCatalog(comment='--', proposer='Leathaud', propid='2020B-0288', seqid='F2021B')
+
+    if is_queued is None:
+        # \\ build is_queued <- previously observed objects
+        is_queued = pd.DataFrame ( index=mastercat.index,
+                                columns=['is_queued','qstamp','has_priority'])
+        is_queued['is_queued'] = False
+        is_queued.loc[has_observed, 'is_queued'] = True
+
+    # \\ Define the observatory site -- default is CTIO
+    ctio = observe.ObservingSite ()
+    night_start, night_end = ctio.get_sunriseset ( year, month, day, cut_at_contract=cut_at_contract )
+    if cut_at_contract:    
+        ten_start, ten_end = ctio.get_sunriseset ( year, month, day, alt=-10, cut_at_contract=False)
+        midpoint = ten_start + 0.5*(night_end-ten_start)
+    else:
+        midpoint = night_start + 0.5*(night_end-night_start)
+    if slot == 0:
+        print('[predict] night slot: Full night')
+        obs_start = night_start
+        obs_end = night_end
+    elif slot == 1:
+        print('[predict] night slot: First half')
+        obs_start = night_start
+        obs_end = midpoint
+    elif slot==2:
+        print('[predict] night slot: Second half')
+        obs_start = midpoint
+        obs_end = night_end 
+    elif slot==3:
+        print('[predict] night slot: Second 3/4')
+        obs_start = night_start + 0.5*(midpoint - night_start)
+        obs_end = night_end
+    elif slot==4:
+        if extra == 1:
+            s = ''
+        else:
+            s = 's'
+        print (f'[predict] night slot: Second half and the last {extra} hour{s} of the first half')
+        obs_start = midpoint - datetime.timedelta(hours=extra)
+        obs_end = night_end
+        
+
+    print(f"obsStart: {obs_start.astimezone(ctio.timezone).strftime(fmt)} Santiago")
+    print(f"          {obs_start.astimezone(et).strftime(fmt)} ET")
+    print(f"          {obs_start.astimezone(pt).strftime(fmt)} PT")
+    print(f"          {obs_start.strftime(fmt)} UTC")
+    print(f"obsEnd:   {obs_end.astimezone(ctio.timezone).strftime(fmt)} Santiago")
+    print(f"          {obs_end.astimezone(et).strftime(fmt)} ET")
+    print(f"          {obs_end.astimezone(pt).strftime(fmt)} PT")
+    print(f"          {obs_end.strftime(fmt)} UTC")
+    
+    moon_cillum, moon_altreport = ctio.track_moon ( obs_start, obs_end)
+    print(f'Moon illumination is: {moon_cillum:.2f}')
+    print(f'Moon max altitude during observation is: {moon_altreport:.2f}')
+
+    is_queued_tmrw = ocat.plan_night ( obs_start, ctio, catalog=mastercat, obs_end=obs_end,
+                                     is_queued=is_queued.copy(),
+                                     maxairmass=1.5, 
+                                     object_priority=priorities,
+                                     **kwargs )
+
+    print_backupaltitudes (obs_start, obs_end )
+    nextbackupscript ( tele )
+    return is_queued_tmrw
---
+++ scripts/sigjson_s2022a.py
@@ -0,0 +1,189 @@
+import sys
+import numpy as np
+import pandas as pd
+from skipper import planner
+
+import make_pointings
+
+obsdir="/Users/kadofong/Google Drive/My Drive/MerianSurvey/observing/DataLogs/"
+priorities = {'COSMOS':0,'GAMA':3, 'GAMAhigh':2, 'GAMAearly':1, 'GAMAlate':3}
+
+######################### ==>
+dates = [ (2022,2,idx) for idx in range(2,11)]
+dates += [ (2022,2,28) ]
+dates += [ (2022,3,idx) for idx in range(1,10)]
+#dates = np.asarray(dates)
+
+slots = [ 4 for idx in range(2,6) ] # first Feb nights, 2/28, and 3/1
+slots += [ 2 for idx in range(6,13)]
+slots += [ 3 for idx in range(2,10) ] # second 3/4 for rest of run
+slots = np.asarray(slots)
+
+filters = [ 'N540' for idx in range(2,16) ]
+filters += [ 'N708' for idx in range(5,10) ]
+filters = np.asarray(filters)
+
+
+assert len(filters) == len(slots)
+assert len(slots) == len(dates)
+
+nightslot_d = dict ( [(key,val) for key,val in zip(dates,slots) ] )
+filter_d = dict ( [ (key,val) for key, val in zip(dates,filters) ] )
+######################### <==
+
+def whichfield ( year, month, day ):
+    tpl = (year,month,day)
+    field = 'COSMOSGAMA'
+    mfilt = filter_d[tpl]
+    nightslot = nightslot_d[tpl]
+    return mfilt, field, nightslot
+
+def _load_mastercat_cosmos ( fname = '../pointings/cosmosgama_N540.csv' ):
+    mastercat = pd.read_csv ( fname )
+    mastercat = mastercat.set_index('object')
+    mastercat['wait'] = "False"
+    mastercat['proposer'] = 'Leauthaud'
+    mastercat = mastercat.drop('object.1', axis=1)
+    mastercat['object'] = mastercat.index
+    return mastercat
+
+def load_mastercat ():
+    # \\ load Shany's pointings (expanded for S21A HSC coverage)
+    halpha_s2022a = pd.read_csv('../pointings/gama_2022A.csv', index_col='object.1')
+
+    # \\ load Shany's old pointings
+    oiii_pointings = _load_mastercat_cosmos ()
+    halpha_pointings = _load_mastercat_cosmos ( '../pointings/S2021A.csv')
+    
+    # \\ add priorities to GAMA field
+    is_high = (halpha_s2022a['dec'] > 1.5)
+    is_early = (halpha_s2022a['RA'] < 160.)
+    is_late = (halpha_s2022a['RA'] > 210.)
+
+    halpha_s2022a['priority_name'] = 'GAMA'
+    halpha_s2022a.loc[is_high&~is_early, 'priority_name'] = 'GAMAhigh'
+    halpha_s2022a.loc[is_early, 'priority_name'] = 'GAMAearly'
+    halpha_s2022a.loc[is_late, 'priority_name'] = 'GAMAlate'    
+    
+    # \\ copy & modify Halpha catalog to be OIII catalog
+    get_catalog_objects = lambda x: x['object'].str.extract(r'(.*?(?=_))')[0]
+    oiii_s2022a = halpha_s2022a.copy()
+    oiii_s2022a['filter'] = 'N540'
+    oiii_s2022a['object'] = [ xo.replace('N708','N540') for xo in oiii_s2022a['object'] ]
+    oiii_s2022a.index = oiii_s2022a['object']
+    oiii_s2022a['expTime'] = 900.
+
+    # \\ add COSMOS pointings
+    halpha_cosmos = halpha_pointings.loc[get_catalog_objects(halpha_pointings)=='COSMOS'].copy()
+    halpha_cosmos['priority_name'] = 'COSMOS'
+    halpha_s2022a = pd.concat([halpha_s2022a, halpha_cosmos], sort=False)                                   
+
+    oiii_cosmos = oiii_pointings.loc[get_catalog_objects(oiii_pointings)=='COSMOS'].copy()
+    oiii_cosmos['priority_name'] = 'COSMOS'
+    oiii_s2022a = pd.concat([oiii_s2022a, oiii_cosmos], sort=False)    
+    
+    return halpha_s2022a, oiii_s2022a
+
+def write_backupjson ():
+    from astropy import coordinates
+    for filt in ['g','r']:
+        center = coordinates.SkyCoord ("35.739030633438745 -4.7489828727193775", unit='deg')
+        catalog_l, ocat, frd = make_pointings.build_backup (filter=filt, center=center, name='SXDS')
+
+        for ij in np.arange(10):
+            fp = f'../json/backup_scripts/SXDS_5minAGN_{filt}_{ij+1:02d}.json'
+            print(fp)
+            ocat.to_json ( catalog_l[ij], fp=fp, 
+                           end_with_onemin=False )
+
+ 
+def plan_tomorrow ( day, month, year, tele_fname, copilot_fname, mfilt=None, **kwargs ):        
+    halpha_s2022a, oiii_s2022a = load_mastercat ()
+    if mfilt is None:
+        mfilt, _, slot = whichfield ( year, month, day )
+        if slot == 4:
+            extra = 1. # ONE extra hour from Soo
+        else:
+            extra= None
+    
+    if mfilt == 'N540':
+        mastercat = oiii_s2022a
+    elif mfilt == 'N708':
+        mastercat = halpha_s2022a
+    else:
+        raise ValueError (f"Filter {mfilt} not recognized.")
+    
+    is_queued = planner.plan_tomorrow ( day, month, year, tele_fname, copilot_fname, mastercat, whichfield=whichfield,
+                   priorities=priorities, extra=extra, **kwargs )
+    return is_queued
+
+                   
+def predict_all ( tele_fname=None, copilot_fname=None ):
+    # \\\ Remaining nights of F2021B run
+    rdates = [(2022,1,idx) for idx in range(25,32) ]
+    rdates = np.asarray(rdates)    
+        
+    # \\ Load pointings, observation logs 
+    _, oiii_s2022a = load_mastercat ()   
+    if tele_fname is None:
+        tele_fname = f'{obsdir}/tele20220112.csv'
+    if copilot_fname is None:
+        copilot_fname = f'{obsdir}/db_merian.fits'
+    
+    # \\ Predict remaining nights in F2021B
+    is_queued_oiii = None
+    is_queued_halpha = None
+    for date in rdates:
+        is_queued_oiii = planner.plan_tomorrow ( date[2],date[1],date[0], tele_fname, copilot_fname, oiii_s2022a, 
+                                                current_field='COSMOSGAMA',
+                                                current_filter='N540',                                                
+                                                current_slot = 2,
+                                                priorities=priorities,
+                                                is_queued=is_queued_oiii,
+                                                pad_last_hour=False,
+                                                save=False)
+        print('\n<(-.-)>\n')
+    
+    for date in dates:
+        mfilt, _, _ = whichfield ( date[0],date[1],date[2] )
+        if mfilt == 'N708':
+            is_queued_halpha = plan_tomorrow ( date[2], date[1], date[0], tele_fname, copilot_fname, 
+                                              is_queued=is_queued_halpha, pad_last_hour=False, save=False )
+        elif mfilt == 'N540':
+            is_queued_oiii = plan_tomorrow ( date[2], date[1], date[0], tele_fname, copilot_fname, 
+                                              is_queued=is_queued_oiii, pad_last_hour=False, save=False )
+        print('\n<(-.-)>\n')
+    
+    return is_queued_oiii, is_queued_halpha
+                
+    
+
+kw_types = {'save':lambda x: x.lower == 'true'}
+
+if __name__ == '__main__':
+    print('='*27)
+    print('== Merian Planner F2021B ==')
+    print('='*27)
+
+
+    if len(sys.argv)==1 or sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        print('S2022A JSON file generation.')
+        print('\n(usage) python sigjson_s2022a.py [run day] [run month] [run year] [path/to/telemetry/output] [path/to/copilot/output]')
+        print('  [run day] integer, the date of the beginning of the night')
+        print('  [path/to/telemetry/output] path to the CSV containing the telemetry output from SISPI')
+        print('(contact kadofong at princeton dot edu for help)\n')
+    else:
+        kwarg_keys = [ x[2:] for x in sys.argv[5::2] ]
+        kwarg_cont = sys.argv[6::2]
+        kwargs = dict(zip(kwarg_keys,kwarg_cont))
+        for key in kwargs:
+            kwargs[key] = kw_types[key](kwargs[key])
+        
+        print(f'DAY:       {sys.argv[1]}')
+        print(f'MONTH:     {sys.argv[2]}')
+        print(f'YEAR:      {sys.argv[3]}')
+        print(f'TELEFILE:  {sys.argv[4]}')
+        print(f'COPILOT:   {sys.argv[5]}')
+        w = open('../resources/aart.txt','r').read()
+        print(w)
+        plan_tomorrow ( int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4], sys.argv[5],**kwargs )
